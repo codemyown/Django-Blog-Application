d38cd80c2b11486d59ff9466e908b3a26cf7ff1f
ajaypawar22112001 <ajay.pawar@reversebits.tech>
2025-04-11 09:44:48 +0530

    test CI/CD pipeline

--- backend/blogapi/settings.py
+++ backend/blogapi/settings.py
@@ -126,6 +126,6 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-# STATIC_URL = "/static/"
+STATIC_URL = "/static/"
 # MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # MEDIA_URL = "/images/products/download/"
